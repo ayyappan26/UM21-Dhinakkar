9db56d58e0974c7090081950da154baad4ff2b89
dhinu1999 <59406677+dhinu1999@users.noreply.github.com>
2021-09-01 13:38:17 +0530

    Apachepoi

---
+++ Apachepoi/src/exceloperation/Excelop.java
@@ -0,0 +1,45 @@
+package exceloperation;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.*;
+
+public class Excelop {
+   public static void main(String [] args) throws IOException
+   {
+	   String file="C:\\Users\\dhinu\\neon workspace\\Apachepoi\\Excelfile\\Book.xlsx";
+	   
+	   FileInputStream fileinput=new FileInputStream(file);
+	   
+	   XSSFWorkbook workbook=new XSSFWorkbook(fileinput);
+	   
+	   XSSFSheet sheet=workbook.getSheet("Sheet1");
+	   
+  
+      int row=sheet.getLastRowNum();
+      int col=sheet.getRow(1).getLastCellNum();
+      
+      for(int r=0;r<row;r++)
+      {
+    	  XSSFRow rows=sheet.getRow(r);
+      for(int c=0;c<col;c++)
+      {
+    	    XSSFCell cols=rows.getCell(c);
+        
+    	    switch(cols.getCellType())
+    	    {
+    	    case STRING:System.out.print("   "  +cols.getStringCellValue());
+    	            break;
+    	    case NUMERIC:System.out.print("   "  +cols.getNumericCellValue());
+    	            break;
+    	    case BOOLEAN:System.out.print("   "  +cols.getBooleanCellValue());
+    	            break;
+    	    
+    	    }
+         }
+       System.out.println();
+}
+}
+}
